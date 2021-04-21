71264af625778da790519ac18c44ed6488cea808
rahul.thawal <iamrahulthawal@gmail.com>
2021-04-21 14:41:45 +0530

    Display in Table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DisplayTable msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DisplayTable from './components/DisplayTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DisplayTable
   }
 }
 </script>
---
+++ src/components/DisplayTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <table>
+  <thead>
+    <tr>
+      <th>INDEX</th>
+      <th>ID</th>
+      <th>TITLE</th>
+      <th>BODY</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(post, index) in postInfo" :key="post.id">
+      <td>{{ index + 1 }}</td>
+      <td>{{ post.id }}</td>
+      <td>{{ post.title }}</td>
+      <td>{{ post.body }}</td>
+    </tr>
+  </tbody>
+</table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+
+  data(){
+        return{
+            postInfo: []
+        };
+    },
+
+  created(){
+      this.getpostInfo();
+  },
+
+  methods:{
+    getpostInfo(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => this.postInfo = response.data)
+        .catch(error => this.postInfo = error.data);
+    },      
+  }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
